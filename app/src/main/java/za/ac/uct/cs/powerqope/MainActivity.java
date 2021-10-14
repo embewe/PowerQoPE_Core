8e69943d521184d6917bfadec8d54c9f4da44b18
staveesh <sharmataveesh@yahoo.in>
2021-10-14 08:59:10 +0200

    Merged with backend-integration

--- app/src/main/java/za/ac/uct/cs/powerqope/MainActivity.java
+++ app/src/main/java/za/ac/uct/cs/powerqope/MainActivity.java
@@ -1,14 +1,20 @@
 package za.ac.uct.cs.powerqope;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.ColorInt;
 import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
@@ -35,8 +41,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.yarolegovich.slidingrootnav.SlidingRootNav;
 import com.yarolegovich.slidingrootnav.SlidingRootNavBuilder;
 
+import java.security.Security;
 import java.util.Arrays;
-
+import java.util.Properties;
 
 
 public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnItemSelectedListener {
@@ -79,7 +86,7 @@ public class MainActivity extends AppCompatActivity implements DrawerAdapter.OnI
                     if (result.getResultCode() == Activity.RESULT_OK) {
                         startDNSSvc();
                     } else{
-                        Log.e(TAG, "VPN dialog not accepted!\r\nPress restart to display dialog again!");
+                        Log`.e(TAG, "VPN dialog not accepted!\r\nPress restart to display dialog again!");
                     }
                 }
             });
